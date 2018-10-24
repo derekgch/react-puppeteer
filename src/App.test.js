f6cd72eb8849fcb1668c29ab5b80608e70135c2b
Derek Chen <derekgc@icloud.com>
2018-10-24 17:50:23 -0400

    add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -36,4 +36,41 @@ describe('H1 Text', () => {
 
 	browser.close();
   }, 16000);
-});
+},
+
+describe('Contact Form', () => {
+  test('Can submit contact form', async () => {
+	let browser = await puppeteer.launch({
+	  headless: false,
+	  devtools: true,
+	  slowMo: 25
+	});
+	let page = await browser.newPage();
+
+	page.emulate({
+	  viewport: {
+		width: 500,
+		height: 900
+	  },
+	  userAgent: ''
+	});
+
+	await page.goto('http://localhost:3000/');
+	await page.waitForSelector('.contact-form');
+	await page.click("input[name=fullname]");
+	await page.type("input[name=fullname]", person.name);
+	await page.click("input[name=email]");
+	await page.type("input[name=email]", person.email);
+	await page.click("textarea[name=message]");
+	await page.type("textarea[name=message]", person.message);
+	await page.click("input[type=checkbox]");
+
+	await page.click("input[name=question]");
+  const html = await page.$eval('input[name=fullname]', e => e.innerHTML);
+	expect(html).toBe(person.name);
+
+	browser.close();
+  }, 9000000);
+})
+
+);
